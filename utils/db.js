b21837d52a380501a355ed5bf3a24f5b12cb6b0f
phila-hh <filimon.haftomh@gmail.com>
2024-03-13 10:29:38 -0700

    done: task 1

---
+++ utils/db.js
@@ -0,0 +1,38 @@
+import envLoader from './env_loader';
+import mongodb from 'mongodb';
+import Collection from 'mongodb/lib/collection';
+
+class DBClient {
+  constructor() {
+    envLoader();
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const db = process.env.DB_DATABASE || 'files_manager';
+    const dbURL = `mongodb://${host}:${port}/${db}`;
+    this.client = new mongodb.MongoClient(dbURL, { useUnifiedTopology: true });
+    this.client.connect();
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    return this.client.db().collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
+
+  async usersCollection() {
+    return this.client.db().collection('users');
+  }
+
+  async filesCollection() {
+    return this.client.db().collection('files');
+  }
+}
+
+export const dbClient = new DBClient();
+export default dbClient;
