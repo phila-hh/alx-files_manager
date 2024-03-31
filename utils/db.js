65e058707d32a80e18947ce18619a8b090eb63f0
phila-hh <filimon.haftomh@gmail.com>
2024-03-31 07:01:58 -0700

    db.js fix

--- utils/db.js
+++ utils/db.js
@@ -7,8 +7,9 @@ class DBClient {
     envLoader();
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
-    const db = process.env.DB_DATABASE || 'files_manager';
-    const dbURL = `mongodb://${host}:${port}/${db}`;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const dbURL = `mongodb://${host}:${port}/${database}`;
+
     this.client = new mongodb.MongoClient(dbURL, { useUnifiedTopology: true });
     this.client.connect();
   }
